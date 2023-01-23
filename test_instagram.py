b2b6ca45a5dfff7fd00251e0af02ca41d93c99eb
Guilherme Morone <guimorone@gmail.com>
2023-01-23 18:55:49 -0300

    using logging lib

--- test_instagram.py
+++ test_instagram.py
@@ -2,11 +2,14 @@ import pandas as pd
 import sys
 import traceback
 import timeit
+import logging
 from random import randint
 from time import sleep
 
 import instaloader
-from instaloader.exceptions import TwoFactorAuthRequiredException, ConnectionException
+from instaloader.exceptions import TwoFactorAuthRequiredException
+
+logging.basicConfig(level=logging.INFO)
 
 
 class MyRateController(instaloader.RateController):
@@ -15,7 +18,7 @@ class MyRateController(instaloader.RateController):
 
     def wait_before_query(self, query_type):
         # Get random sleep time
-        sleep_per_req = randint(5, 15)
+        sleep_per_req = randint(10, 20)
         sleep(sleep_per_req)
 
         return
@@ -24,12 +27,14 @@ class MyRateController(instaloader.RateController):
 def main(username: str, password: str, L: instaloader.Instaloader):
     def login(user: str, passwd: str):
         try:
-            print("Logging in...")
+            logging.info("Logging in...")
             L.login(user, passwd)
         except TwoFactorAuthRequiredException:
             code = input("Verification Code: ")
             while not code:
-                print("--- Error while trying to send the verification code ---")
+                logging.error(
+                    "--- Error while trying to send the verification code ---"
+                )
                 code = input("Try again: ")
             L.two_factor_login(code)
 
@@ -37,42 +42,43 @@ def main(username: str, password: str, L: instaloader.Instaloader):
 
     profile_to_fetch = input("Profile to fetch (skip to fetch the logged account): ")
     profile_to_fetch = profile_to_fetch if profile_to_fetch else username
-    print(f"Loading {profile_to_fetch} profile from an Instagram handle...")
+    logging.info(f"Loading {profile_to_fetch} profile from an Instagram handle...")
     profile = instaloader.Profile.from_username(L.context, profile_to_fetch)
 
-    print("Retrieving the usernames of all followers and followings...")
+    logging.info("Retrieving the usernames of all followers and followings...")
     followers = [follower.username for follower in profile.get_followers()]
     followings = [followee.username for followee in profile.get_followees()]
 
-    print("Retrieving the usernames of all people that do not follow back...")
+    logging.info("Retrieving the usernames of all people that do not follow back...")
     people_that_do_not_follow_back = list(
         filter(lambda username: username not in followers, followings)
     )
 
-    print("Converting the data to a DataFrame...")
+    logging.info("Converting the data to a DataFrame...")
     followers_df = pd.DataFrame(followers)
     followings_df = pd.DataFrame(followings)
     people_that_do_not_follow_back_df = pd.DataFrame(people_that_do_not_follow_back)
 
-    print("Storing the results in a CSV file...")
+    logging.info("Storing the results in a CSV file...")
     followers_df.to_csv("followers.csv", index=False)
     followings_df.to_csv("followings.csv", index=False)
     people_that_do_not_follow_back_df.to_csv(
         "people_that_do_not_follow_back.csv", index=False
     )
 
-    print("-------------- NUMBERS --------------")
-    print("Followers:", len(followers))
-    print("Followings:", len(followings))
-    print("People that do not follow back:", len(people_that_do_not_follow_back))
-    print("-------------------------------------")
+    logging.info("-------------- NUMBERS --------------")
+    logging.info("Followers:", len(followers))
+    logging.info("Followings:", len(followings))
+    logging.info("People that do not follow back:", len(people_that_do_not_follow_back))
+    logging.info("-------------------------------------")
 
 
 if __name__ == "__main__":
     start = timeit.default_timer()
 
     if len(sys.argv) != 3:
-        print("Set your credentials properly and try again")
+        logging.warning(f"Usage: `python {sys.argv[0]} USERNAME PASSWORD`")
+        logging.critical("Set your credentials properly and try again!")
     else:
         username = sys.argv[1]
         password = sys.argv[2]
@@ -85,4 +91,4 @@ if __name__ == "__main__":
             L.close()
 
     stop = timeit.default_timer()
-    print("Runtime: ", stop - start)
+    logging.info(f"Runtime: {stop - start} seconds")
